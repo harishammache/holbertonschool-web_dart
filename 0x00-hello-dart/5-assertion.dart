0fef027b7e28d735142feaf5043f5ca6aceb12d1
harishammache <hammache_haris@hotmail.fr>
2026-02-26 12:45:26 +0100

    Replace assertion for passing score with print statement in 5-assertion.dart

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -2,5 +2,5 @@ void main(List<String> args) {
     int score = int.parse(args[0]);
 
     assert(score >= 80, "The score must be bigger or equal to 80");
-    assert(score < 80, "You Passed\n");
+    print("You Passed\n");
 }
