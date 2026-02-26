328b08109373486785fcac4d2ff9e07a18ac50a3
harishammache <hammache_haris@hotmail.fr>
2026-02-26 12:46:23 +0100

    Remove newline from print statement in 5-assertion.dart

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,6 +1,6 @@
 void main(List<String> args) {
     int score = int.parse(args[0]);
 
-    assert(score >= 80, "The score must be bigger or equal to 80");
-    print("You Passed\n");
+    assert(score >= 80, "The score must be bigger or equal to 80\n");
+    print("You Passed");
 }
