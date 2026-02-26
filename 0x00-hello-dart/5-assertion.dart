64510cf070d2f0506e4412024241859c5c7e75ad
harishammache <hammache_haris@hotmail.fr>
2026-02-26 12:44:49 +0100

    Fix assertion messages in 5-assertion.dart for clarity

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,6 +1,6 @@
 void main(List<String> args) {
     int score = int.parse(args[0]);
 
-    assert(score >= 80, "You Passed");
-    assert(score <80, "The score must be bigger or equal to 80\n");
+    assert(score >= 80, "The score must be bigger or equal to 80");
+    assert(score < 80, "You Passed\n");
 }
