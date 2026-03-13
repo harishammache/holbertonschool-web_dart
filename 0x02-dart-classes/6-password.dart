b531b59a4ea863d139d295fe2b06b661e61d6aff
harishammache <hammache_haris@hotmail.fr>
2026-03-13 12:29:14 +0100

    Add User and Password classes with JSON serialization and validation methods

---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,29 @@
+class Password{
+    String _password = "";
+
+    Password({String password = ""}) {
+        _password = password;
+    }
+
+    String get password => _password;
+
+    set password(String value) {
+        _password = value;
+    }
+
+    bool isValid(){
+        if (_password.length >= 8 && 
+        _password.length <= 16 &&
+        _password.contains(RegExp(r'[A-Z]')) &&
+        _password.contains(RegExp(r'[a-z]')) && 
+        _password.contains(RegExp(r'[0-9]'))
+        ){
+            return true;
+        }
+        return false;
+    }
+    
+    String toString(){
+        return "Your Password is: ${_password}";
+    }
+}
