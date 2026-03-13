9a98dddef2a0789859b206b0c1dcd96cda5315c6
harishammache <hammache_haris@hotmail.fr>
2026-03-13 12:50:14 +0100

    Refactor User class to improve password handling and maintain consistency in method formatting

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,29 +1,40 @@
 import '6-password.dart';
 
-class User extends Password{
-    int id;
-    String name;
-    int age;
-    double height;
-    String user_password;
+class User extends Password {
+  int id;
+  String name;
+  int age;
+  double height;
 
-    User({required this.id, required this.name, required this.age, required this.height, required this.user_password});
+  User({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.height,
+    String user_password = "",
+  }) : super(password: user_password);
 
-    Map <String, dynamic> toJson() {
-        return {"id": id, "name": name, "age": age, "height": height};
-    }
+  String get user_password => password;
 
-    static User fromJson(Map<dynamic, dynamic> userJson) {
-        return User (
-            id: userJson['id'],
-            name: userJson['name'],
-            age: userJson['age'],
-            height: userJson['height'],
-            user_password: userJson['user_password']
-        );
-    }
+  set user_password(String value) {
+    super.password = value;
+  }
 
-    String toString() {
-        return "User(id : $id ,name: $name, age: $age, height: $height, Password: ${isValid()})";
-    }
+  Map<String, dynamic> toJson() {
+    return {"id": id, "name": name, "age": age, "height": height};
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height'],
+      user_password: userJson['user_password'] ?? "",
+    );
+  }
+
+  String toString() {
+    return "User(id : $id ,name: $name, age: $age, height: $height, Password: ${isValid()})";
+  }
 }
