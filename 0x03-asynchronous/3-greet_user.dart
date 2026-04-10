bef71ef67083af4464cf26290a5aca22d5aa41cf
harishammache <hammache_haris@hotmail.fr>
2026-04-10 13:10:47 +0200

    Add greetUser and loginUser functions to handle user greeting and login logic asynchronously

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,27 @@
+import "3-util.dart";
+
+Future<String> greetUser() async {
+    try {
+        String user = await fetchUserData();
+        Map<String, dynamic> userMap = jsonDecode(user)
+        return "Hello ${userMap['username']} ."
+    }
+    catch (e) {
+        return ("error caught: $e")
+    }
+}
+
+Future<String> loginUser() async {
+    try {
+        bool userlog = await checkCredentials();
+        print("There is a user: $userlog");
+        if (userlog) {
+            return await greetUser();
+        } else {
+            return "Wrong credentials",
+        }
+    }
+    catch (e) {
+        return "error caught: $e";
+    }
+}
