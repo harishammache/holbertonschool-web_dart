205f83060ba6f32b83c2b666e71070b79d362edb
harishammache <hammache_haris@hotmail.fr>
2026-04-10 13:44:54 +0200

    Add calculateTotal function to compute total from user orders asynchronously

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,24 @@
+import "4-util.dart";
+import "dart:convert";
+
+Future<int> calculateTotal() async {
+    try {
+        String userData = await fetchUserData();
+        Map<String, dynamic> userMap = jsonDecode(userData);
+        String userId = userMap['id'];
+
+        String ordersJson = await fetchUserOrders(userId);
+        List<dynamic> orders = jsonDecode(ordersJson);
+
+        double total = 0;
+        for (var product in orders) {
+            String priceJson = await fetchProductPrice(product);
+            double prince = jsonDecode(priceJson);
+            total += price;
+        }
+    }
+    return total;
+    catch (e){
+        return -1;
+    }
+}
